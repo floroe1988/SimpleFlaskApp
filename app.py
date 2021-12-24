5c07a0a5347e8c7ab823f6451d142ed319c36bc8
floroe1988 <68111417+floroe1988@users.noreply.github.com>
2021-12-24 13:33:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    world = "World"
-    html = f"<h3>Hello {world}</h3>"
-    return html.format(format)
+    html = <h3>Hello World</h3>"
+    return html
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
